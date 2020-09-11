7f0259a9b40c3d9c8bb68dd273dcec09f85e126d
Mauricio Cartagena <mc973220@gmail.com>
2020-09-11 14:25:11 -0400

    Formas de agregar un nuevo valor a un array

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React,{ useState } from 'react';
 //snipet para crear todo de una rafc 
 
 const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () =>{
+       
+        //Es una forma de agregar al array nuevos valores
+        setCategories( [ ...categories, 'Hunter' ] );
+
+
+        //Es una forma de agregar al array nuevos valores pero inversa ahora
+        // setCategories( [ 'Hunter' , ...categories ] );
+
+        //Otra forma de agregar
+        // setCategories( cats => [ ...cats,'Hunter' ]);
+
+    }
+
 
     return (
 
         <React.Fragment>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={ handleAdd }>Agregar</button>
+
+
+            <ol>
+                {
+                    categories.map(( category ) => {
+                    return <li key={ category }> { category }</li>;
+                    })
+                }
+            </ol>
+
+
         </React.Fragment>
     )
 
