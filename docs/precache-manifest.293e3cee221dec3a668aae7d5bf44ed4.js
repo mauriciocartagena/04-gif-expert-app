b047944ebc30564ec7c0f84cf69d049be9cf1448
Mauricio Cartagena <mc973220@gmail.com>
2020-09-12 15:33:22 -0400

    docs agregado

---
+++ docs/precache-manifest.293e3cee221dec3a668aae7d5bf44ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ec96cf3ae228f1bf0d2a93c02a2a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf9745c1190a32649ddb",
+    "url": "/static/css/main.b1a09336.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9745c1190a32649ddb",
+    "url": "/static/js/main.85968ae2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
